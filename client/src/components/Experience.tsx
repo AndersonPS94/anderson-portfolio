6d5eb9d40a87d2c16af58a22f644ab7b30eb68be
Anderson Santos <69472748+AndersonPS94@users.noreply.github.com>
2026-08-15 07:27:43 -0300

    Update image source to use BASE_URL

--- client/src/components/Experience.tsx
+++ client/src/components/Experience.tsx
@@ -47,7 +47,7 @@ export default function Experience() {
               <div className="relative">
                 <div className="h-16 w-16 rounded-lg border border-white/10 bg-white p-1.5 flex items-center justify-center">
                   <img
-                    src="/assets/solution-ti-logo_6df50ae1.png"
+                    src={`${import.meta.env.BASE_URL}/assets/solution-ti-logo_6df50ae1.png`}
                     alt="Logo da Solution TI"
                     className="h-full w-full object-contain"
                     loading="lazy"
